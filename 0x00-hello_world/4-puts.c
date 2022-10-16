6c11ca9c6bdf2d6ad185748dd3c31013d0fe231c
Bensonuriah <Bensonuriah@gmail.com>
2022-10-16 07:23:48 -0700

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+nclude<stdio.h>
+
+/**
+ *
+ * * main - program prints with function puts
+ *
+ * *
+ *
+ * *Return: Always 0
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
