98c82e0b138b3191b232f2dda8bb760ed4739f47
Bensonuriah <Bensonuriah@gmail.com>
2022-10-17 13:30:42 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints alphabets
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+
