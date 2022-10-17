118c23fac708cddace03abb745cf26e3eae72db7
Bensonuriah <Bensonuriah@gmail.com>
2022-10-17 02:59:13 -0700

    varibles

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-#include <stdio.h>
-/**
- *   main - Entry point
- * Return: Always 0 (Success)
- */
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n, lastDigit;
