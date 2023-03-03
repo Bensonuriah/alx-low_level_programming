ac5e32124a57263531a7b5632fc34dccb9a5f594
bensonuriah <bensonuriah@gmail.com>
2023-03-02 19:30:27 -0800

    two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - function that concatenates two strings.
+ *
+ * appends src
+ * appends dest
+ * Return: a concatenates string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+
+ {
+	strcat (dest, src);
+	return (dest);
+
+ } 
+
+
