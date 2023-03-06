a1e9f5e8bc38c1e1459e46ac4a9acd965a015c07
bensonuriah <bensonuriah@gmail.com>
2023-03-06 13:01:47 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- *Main - This function will memory with a constant byte.
- *b will be the constant to be filled in the memory block
- * 
+ * Main - This function will memory with a constant byte.
+ * b - the constant to be filled in the memory 
  * Return Always - 0
  */
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
 	unsigned int i;
 	
 	for (i = 0; i < n; i++)
-	s[i] = b;
+	
+		s[i] = b;
 	return (s);
 }
 	
