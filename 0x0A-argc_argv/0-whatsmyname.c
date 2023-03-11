bd3566ae55a7e847123e9cef4263cc890d7e9164
bensonuriah <bensonuriah@gmail.com>
2023-03-11 03:35:28 -0800

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - will print the name of the program
+ * @argc : count number of arguments
+ * @argv : pointer to the strings
+ * Return: Awalys(0)
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef maih_h
+#define maih_h
+
+int main (int argc, char *argv);
+int _putchar(int c);
+#endif
