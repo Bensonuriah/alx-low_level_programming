4087d55969c2e2d469dede7ec36c420cbe75381e
bensonuriah <bensonuriah@gmail.com>
2023-03-21 04:01:06 -0700

    inclusion

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main: prints minimum number.
+ * @argc:counts numbers of argument in command line.
+ * @argv: array of strings in command line.
+ * Return: returns (1) or (0).
+ */
+int man (int argc, char *argv[])
+{
+	int cents, nchange = 0; 
+	if (argc == 1 || agrc > 2;)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	else if (argc < 0;)
+	{
+	printf("0\n");
+	}
+	cents = atoi(argv[1]);
