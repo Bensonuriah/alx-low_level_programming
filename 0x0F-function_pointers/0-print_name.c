d18ccde2b00fece8b71443b55a395226bd4e2b79
Your Name <bensonuriah@gmail.com>
2023-06-21 16:52:47 -0700

    correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
-#include <function_pointers.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
- *print_name - prints a name
+ * print_name - prints a name
  * @name: name to print
  * @f: pointer to the printing function
  */
